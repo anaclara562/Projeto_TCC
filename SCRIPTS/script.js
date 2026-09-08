901636a1e32f30d774c061d357c93f2bf7dbb69a
anaclara562 <anaclaraa.rois2@gmail.com>
2026-09-08 16:12:49 -0300

    decimo commit

--- SCRIPTS/script.js
+++ SCRIPTS/script.js
@@ -1,17 +1,23 @@
 function mostrarInfo(idDesejado) {
 
-    // Mostra o quadro
     const quadro = document.querySelector('.quadro-info');
+    const conteudoSelecionado = document.getElementById(idDesejado);
+
+    // Se o conteúdo já estiver aparecendo, esconde
+    if (conteudoSelecionado.classList.contains('ativo')) {
+        conteudoSelecionado.classList.remove('ativo');
+        quadro.classList.remove('ativo');
+        return;
+    }
+
+    // Mostra o quadro
     quadro.classList.add('ativo');
 
     // Esconde todos os conteúdos
-    const todosOsConteudos = document.querySelectorAll('.conteudo');
-
-    todosOsConteudos.forEach(conteudo => {
+    document.querySelectorAll('.conteudo').forEach(conteudo => {
         conteudo.classList.remove('ativo');
     });
 
-    // Mostra o conteúdo selecionado
-    const conteudoSelecionado = document.getElementById(idDesejado);
+    // Mostra o conteúdo clicado
     conteudoSelecionado.classList.add('ativo');
 }
